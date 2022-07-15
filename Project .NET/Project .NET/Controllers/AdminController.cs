40a1073174e40d4e14700995db57bc6f28df42bc
NguyenDuyLong2021 <19130128@st.hcmuaf.edu.vn>
2022-07-15 08:56:40 +0700

    admin

--- Project .NET/Project .NET/Controllers/AdminController.cs
+++ Project .NET/Project .NET/Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project.NET.CSDL;
+using Project.NET.Models;
 
 namespace Project.NET.Controllers
 {
@@ -11,6 +13,13 @@ namespace Project.NET.Controllers
         // GET: Admin
         public ActionResult Index()
         {
+
+            StoreDao storeDao = new StoreDao();
+            int[] inventoryAndImport = storeDao.getInventoryAndImport();
+            ViewBag.inventory = inventoryAndImport[0];
+            ViewBag.import = inventoryAndImport[1];
+            List<ItemStore> itemStores = storeDao.getListStore(0, 1);
+            ViewBag.store = itemStores;
             return View();
         }
     }
