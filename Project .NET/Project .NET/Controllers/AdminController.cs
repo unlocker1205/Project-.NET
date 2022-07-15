7d0413f376746e66de5ed66c8eadb479defde7b0
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-07-15 08:36:11 +0700

    fix

--- Project .NET/Project .NET/Controllers/AdminController.cs
+++ Project .NET/Project .NET/Controllers/AdminController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project.NET.CSDL;
+using Project.NET.Models;
 
 namespace Project.NET.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        public static List<ProductModel> getProductForPage(int page, int numOfProducts)
         {
+            int end = page * numOfProducts;
+            int start = end - numOfProducts;
+            return ProductDao.getAllProduct(numOfProducts, start);
+
+        }
+
+        public static int getNumOfPage(int numOfProduct)
+        {
+            int totalProducts = ProductDao.getTotalPage();
+            int endPage = totalProducts / numOfProduct;
+            if (totalProducts % numOfProduct != 0)
+            {
+                endPage++;
+            }
+            return endPage;
+        }
+        // GET: Admin
+        public ActionResult Index(String param, String page)
+        {
+            if (page == null)
+            {
+                page = "1";
+            }
+            int endPage = getNumOfPage(20);
+            ViewBag.currentPage = Int32.Parse(page);
+            ViewBag.endPage = endPage;
+            List<ProductModel> products = getProductForPage(Int32.Parse(page), 20);
+            ViewBag.products = products;
+
+            if (param != null)
+            {
+
+                if (param.Equals("thongtinlaptop"))
+                {
+                    products = getProductForPage(Int32.Parse(page), 20);
+                    ViewBag.products = products;
+                }
+            }
             return View();
         }
     }
